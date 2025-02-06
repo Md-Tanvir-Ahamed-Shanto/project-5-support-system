ad0228d5ccf38b81dbab88570b7891c8fb20bdda
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-02-06 21:48:59 +0600

    yestarday update push

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-// Backend: Express with MySQL
 const express = require('express');
 const mysql = require('mysql2');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Database Connection
 const db = mysql.createConnection({
