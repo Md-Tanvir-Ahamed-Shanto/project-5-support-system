d96c82a0a4c48f7747a2195c1caaa84d3150a7b7
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-05-14 21:58:34 +0600

    Update API and base URLs to use the production domain

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,6 @@
-export const api_url = "http://localhost:5000"
-export const base_url = "http://localhost:5000/api"
+// export const api_url = "http://localhost:5000"
+// export const base_url = "http://localhost:5000/api"
 
 
-// export const api_url = "http://agent-submission-api.redsqyare.com/"
-// export const base_url = "http://agent-submission-api.redsqyare.com/api"
+export const api_url = "https://agent-submission-api.redsqyare.com/"
+export const base_url = "https://agent-submission-api.redsqyare.com/api"
