2c9f6da84b5eb4c340015ffe12b5631a72bf2c35
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-05-14 23:36:14 +0600

    Update API and base URLs to point to the ticket API

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,5 +2,5 @@
 // export const base_url = "http://localhost:5000/api"
 
 
-export const api_url = "https://agent-submission-api.redsqyare.com/"
-export const base_url = "https://agent-submission-api.redsqyare.com/api"
+export const api_url = "https://ticket-api.redsqyare.com"
+export const base_url = "https://ticket-api.redsqyare.com/api"
